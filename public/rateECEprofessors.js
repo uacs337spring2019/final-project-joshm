7950f843a5ae1e3dce700b5de1b6074b99288f87
joshmassee <49917567+joshmassee@users.noreply.github.com>
2019-04-24 17:07:22 -0700

    Update rateECEprofessors.js

--- public/rateECEprofessors.js
+++ public/rateECEprofessors.js
@@ -22,8 +22,8 @@ construction (list of teachers and teacher profiles.  Allows users to make post
 	};
 
 	function getTeacherList() {
-		var url = "http://localhost:3000?mode=teacherList";
-		//var url = "http://jmrateeceprofessors.herokuapp.com:process.env.PORT?mode=teacherList";
+		//var url = "http://localhost:3000?mode=teacherList";
+		var url = "http://jmrateprofessors2.herokuapp.com?mode=teacherList";
 			fetch(url)
 			.then(checkStatus)
 			.then(function(responseText) {
@@ -79,8 +79,8 @@ construction (list of teachers and teacher profiles.  Allows users to make post
 			let first = names[0];
 			let last = names[1];
 
-			var url = "http://localhost:3000?mode=teacher&first=" + first + "&last=" + last;
-			//var url = "http://jmrateeceprofessors.herokuapp.com?mode=teacher&first=" + first + "&last=" + last;
+			//var url = "http://localhost:3000?mode=teacher&first=" + first + "&last=" + last;
+			var url = "http://jmrateprofessors2.herokuapp.com?mode=teacher&first=" + first + "&last=" + last;
 			fetch(url)
 			.then(checkStatus)
 			.then(function(responseText) {
@@ -294,8 +294,8 @@ construction (list of teachers and teacher profiles.  Allows users to make post
 					'Content-Type' : 'application/json'},
 				body : JSON.stringify(message)};
 
-			let url = "http://localhost:3000";
-			//var url = "http://jmrateeceprofessors.herokuapp.com:process.env.PORT";
+			//let url = "http://localhost:3000";
+			var url = "http://jmrateprofessors2.herokuapp.com";
 
 			//post made with object containing user inputs
 			fetch(url, fetchOptions)
